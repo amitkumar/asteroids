17e39e64f93ee45b8145d89e6f3246ecff8826dc
Amit Kumar <themit@gmail.com>
2016-05-24 21:48:12 -0400

    rotation animation

--- main.js
+++ main.js
@@ -9,9 +9,11 @@ var rectWidth = width/5;
 var rectHeight = height/5;
 
 var x = 0;
-var y = 0 - rectHeight;
+var y = 0;
+var rotationDegrees = 0;
 
 var movementRate = 1;
+var rotationRate = 1;
 
 function draw(){
 	// call clearRect with the entire size of the canvas to clear the whole thing
@@ -20,10 +22,17 @@ function draw(){
 	context.fillStyle = "rgb(0, 0, 0)";
 	context.fillRect(0, 0, width, height);
 	
-	drawTriangle(context, x, y, rectWidth, rectHeight);
+	drawTriangle(context, x, y, rectWidth, rectHeight, rotationDegrees);
+
+	// drawTriangle(context, x * 2, y * 2, rectWidth * 2, rectHeight * 2);
 
 	// Update x and y for the next draw operation
-	y = y + movementRate;
+	// y = y + movementRate;
+	rotationDegrees = rotationDegrees + 1;
+
+	if (rotationDegrees > 360){
+		rotationDegrees = 0;
+	}
 
 	window.requestAnimationFrame(draw);
 }
@@ -31,17 +40,40 @@ window.requestAnimationFrame(draw);
 
 
 
-function drawTriangle(context, x, y, rectWidth, rectHeight){
+function drawTriangle(context, x, y, rectWidth, rectHeight, rotationDegrees){
+	context.save();
+	
+	// var centerOfTriangle = {
+	// 	x : x + rectWidth/2,
+	// 	y : y + rectHeight/2
+	// };
+
+	var centerX = x + rectWidth/2;
+	var centerY = y + rectHeight/2;
+
+	context.translate(centerX, centerY); // Translate to center of rectangle
+	
+	// Convert degrees to radians, because context.rotate needs radians
+	var radians = rotationDegrees * (Math.PI/180);
+	context.rotate(radians); 
+
 	// Draw a triangle with paths
 	context.strokeStyle = 'red';
 	// Start a new path
 	context.beginPath();
+
 	// Now define the path with .moveTo() and .lineTo() methods
-	context.moveTo(x, y);
-	context.lineTo(x + rectWidth, y);
-	context.lineTo(x + rectWidth/2, y + rectHeight);
+	var offsetTriangleX = x - rectWidth/2;
+	var offsetTriangleY = y - rectHeight/2;
+
+	context.moveTo(offsetTriangleX, offsetTriangleY);
+	context.lineTo(offsetTriangleX + rectWidth, offsetTriangleY);
+	context.lineTo(offsetTriangleX + rectWidth/2, offsetTriangleY + rectHeight);
+
 	context.closePath();
 
 	// The path is only made visible when we call .stroke()
 	context.stroke();
+
+	context.restore();
 }

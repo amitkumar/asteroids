441b73a94b0a3c6ac7c4ff45c1d9bf5c71f7a158
Amit Kumar <themit@gmail.com>
2016-06-04 15:12:36 -0400

    moving all rocket-related code to Rocket class

--- main.js
+++ main.js
@@ -18,10 +18,21 @@ var movementRate = 2;
 var rotationRate = 10;
 
 
-var myRocket = new Rocket();
+var myRocket = new Rocket(
+	context,
+	rectWidth, 
+	rectHeight, 
+	rotationDegrees, 
+	rotationRate, 
+	xVector, 
+	yVector,
+	movementRate,
+	x,
+	y,
+	'red');
+
 console.log('myRocket', myRocket);
-myRocket.something = 5;
-console.log('myRocket after changing .something', myRocket);
+
 
 function draw(){
 	// call clearRect with the entire size of the canvas to clear the whole thing
@@ -30,86 +41,11 @@ function draw(){
 	context.fillStyle = "rgb(0, 0, 0)";
 	context.fillRect(0, 0, width, height);
 	
-	x = x + xVector;
-	y = y + yVector;
-
-	drawTriangle(context, x, y, rectWidth, rectHeight, rotationDegrees);
-
-	if (rotationDegrees > 360){
-		rotationDegrees = 0;
-	}
+	myRocket.draw();
 
 	window.requestAnimationFrame(draw);
 }
 window.requestAnimationFrame(draw);
 
 
-window.addEventListener('keydown', function(event) {
-	console.log('keydown event.which', event.which);
 
-	switch(event.which){
-		case 37:
-		console.log('left');
-		rotationDegrees = rotationDegrees - rotationRate;
-		break;
-		case 38:
-		console.log('up');
-		xVector = movementRate * Math.cos(rotationDegrees * Math.PI / 180);
-		yVector = movementRate * Math.sin(rotationDegrees * Math.PI / 180);
-		break;
-		case 39:
-		console.log('right');
-		rotationDegrees = rotationDegrees + rotationRate;
-		break;
-		case 40:
-		console.log('down');
-		xVector = 0;
-		yVector = 0;
-		break;
-		default:
-	}
-});
-
-
-function drawTriangle(context, x, y, rectWidth, rectHeight, rotationDegrees){
-	context.save();
-	
-	var centerX = x + rectWidth/2;
-	var centerY = y + rectHeight/2;
-
-	context.translate(centerX, centerY); // Translate to center of rectangle
-	// console.log('context.translate(centerX, centerY)', centerX, centerY);
-	
-	// context.strokeStyle = 'white';
-	// context.strokeRect(0, 0, width, height);
-
-	// Convert degrees to radians, because context.rotate needs radians
-	var radians = rotationDegrees * (Math.PI/180);
-	context.rotate(radians);
-	// console.log('rotationDegrees', rotationDegrees);
-	// console.log('context.rotate(radians)', radians);
-
-	// context.strokeStyle = 'yellow';
-	// context.strokeRect(0, 0, width, height);
-
-	
-	// Draw a triangle with paths
-	context.strokeStyle = 'red';
-	// Start a new path
-	context.beginPath();
-
-	// Now define the path with .moveTo() and .lineTo() methods
-	var triangleOriginX = -rectWidth/2;
-	var triangleOriginY = -rectHeight/2;
-
-	context.moveTo(triangleOriginX, triangleOriginY);
-	context.lineTo(triangleOriginX + rectWidth, triangleOriginY + rectHeight/2);
-	context.lineTo(triangleOriginX, triangleOriginY + rectHeight);
-
-	context.closePath();
-
-	// The path is only made visible when we call .stroke()
-	context.stroke();
-
-	context.restore();
-}
--- rocket.js
+++ rocket.js
@@ -1,8 +1,124 @@
 class Rocket {
-	constructor() {
+	constructor(
+		context,
+		width, 
+		height, 
+		rotationDegrees, 
+		rotationRate, 
+		xVector, 
+		yVector,
+		movementRate,
+		x,
+		y,
+		color) {
 		// "this" refers to the instance of Rocket that I'm currently working with
 		// Add properties to "this" when each instance of the class should have that property
 
-		this.something = 2;		
+		this.context = context;
+		this.width = width;
+		this.height = height;
+		this.rotationDegrees = rotationDegrees;
+		this.rotationRate = rotationRate;
+		this.xVector = xVector;
+		this.yVector = yVector;
+		this.movementRate = movementRate;
+		this.x = x;
+		this.y = y;
+		this.color = color;
+
+
+		window.addEventListener('keydown', event => this.handleKeydown(event));
+	}
+
+	handleKeydown(event){
+		var rotationDegrees = this.rotationDegrees;
+		var rotationRate = this.rotationRate;
+		var movementRate = this.movementRate;
+		var xVector = this.xVector;
+		var yVector = this.yVector;
+			
+		console.log('keydown event.which', event.which);
+
+		// var rotationDegrees = rotationDegrees;
+
+		switch(event.which){
+			case 37:
+			console.log('left');
+			this.rotationDegrees = rotationDegrees - rotationRate;
+			break;
+			case 38:
+			console.log('up');
+			this.xVector = movementRate * Math.cos(rotationDegrees * Math.PI / 180);
+			this.yVector = movementRate * Math.sin(rotationDegrees * Math.PI / 180);
+			break;
+			case 39:
+			console.log('right');
+			this.rotationDegrees = rotationDegrees + rotationRate;
+			break;
+			case 40:
+			console.log('down');
+			this.xVector = 0;
+			this.yVector = 0;
+			break;
+			default:
+		}
+	}
+
+	update(){
+		// var x = this.x;
+		// x = 2;
+		// this.x did not get changed to 2. Only the variable x got updated.
+		// To change this.x, I have to say this.x = 2;
+		// Variable x only lets me refer to the value of this.x from the time
+		// I did the variable assignment.
+
+		this.x = this.x + this.xVector;
+		this.y = this.y + this.yVector;
+		if (this.rotationDegrees > 360){
+			this.rotationDegrees = 0;
+		}
+	}
+
+	draw(){
+		this.update();
+
+		var context = this.context;
+		var x = this.x;
+		var y = this.y;
+		var width = this.width;
+		var height = this.height;
+		var rotationDegrees = this.rotationDegrees;
+
+		context.save();
+	
+		var centerX = x + width/2;
+		var centerY = y + height/2;
+
+		context.translate(centerX, centerY); // Translate to center of rectangle
+
+		// Convert degrees to radians, because context.rotate needs radians
+		var radians = rotationDegrees * (Math.PI/180);
+		context.rotate(radians);
+		
+		// Draw a triangle with paths
+		context.strokeStyle = this.color;
+		// Start a new path
+		context.beginPath();
+
+		// Now define the path with .moveTo() and .lineTo() methods
+		var triangleOriginX = -width/2;
+		var triangleOriginY = -height/2;
+
+		context.moveTo(triangleOriginX, triangleOriginY);
+		context.lineTo(triangleOriginX + width, triangleOriginY + height/2);
+		context.lineTo(triangleOriginX, triangleOriginY + height);
+
+		context.closePath();
+
+		// The path is only made visible when we call .stroke()
+		context.stroke();
+
+		context.restore();
+
 	}
 }
